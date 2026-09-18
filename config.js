0e9a7a76d66d6ab5f4df16ec14bde29703f3dbb8
wzddl <506577311@qq.com>
2026-09-18 15:25:24 +0800

    新增规则，将部分菜单中的AI 生成、移除背景选项一并加入规则

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 const REMOVE_REF_GUID = [
   { title: "快速 AI 生成…", GUID: "b3a1c7e2-4f58-4d9b-a6e0-8c2f5d7e9b14" },
   { title: "AI 生成", GUID: "6c434810-1fd3-4858-8f7d-0425609fec40" },
+  { title: "AI 生成", GUID: "0fb51fff-d804-416b-b8cf-d5100d6e11a4" },
+  { title: "AI 生成", GUID: "d5596b3a-d5fc-4e36-875c-dbcd1a7d0ae0" },
+  { title: "移除背景", GUID: "dd03eeb6-5a30-44e7-8562-2df6b6095f15" },
   { title: "从云中打开....", GUID: "09360b3d-3317-4375-a128-ca824873441f" },
-  { title: "保存到云...", GUID: "95d7454c-db4a-49d3-aab4-b44a7d886a30" },
   { title: "从云中打开....", GUID: "16bb4eb3-56ed-46c9-b4cf-ecff46a1f3b5" },
+  { title: "保存到云...", GUID: "95d7454c-db4a-49d3-aab4-b44a7d886a30" },
   { title: "保存到云...", GUID: "a66184a5-17e1-4a77-8f36-849c94b3c7a7" },
 ];
 export { REMOVE_REF_GUID };
